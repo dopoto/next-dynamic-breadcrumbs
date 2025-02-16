bade37ae5e7fdbc561811afc40181a4872ac1ae4
dopoto <dopoto@gmail.com>
2025-02-16 09:04:39 +0200

    Fix ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type ReactNode } from "react";
 
-export default function RootLayout(props: {
-  children: ReactNode;
-  modal: ReactNode;
-}) {
+export default function RootLayout(props: { children: ReactNode }) {
   return (
     <html lang="en" className={`${GeistSans.variable} p-4`}>
       <body>{props.children}</body>
